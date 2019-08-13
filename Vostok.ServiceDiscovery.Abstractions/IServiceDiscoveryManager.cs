f6218a0a9cb24f0918373a6894466b6ca01c1bc3
deniaa <deniaa@skbkontur.ru>
2019-08-13 19:11:14 +0500

    Add first base interface.

---
+++ Vostok.ServiceDiscovery.Abstractions/IServiceDiscoveryManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+
+namespace Vostok.ServiceDiscovery.Abstractions
+{
+    [PublicAPI]
+    public interface IServiceDiscoveryManager
+    {
+        [NotNull]
+        Task<IReadOnlyList<string>> GetAllEnvironmentsAsync();
+
+        [NotNull]
+        Task<IReadOnlyList<string>> GetAllApplicationsAsync(string environment);
+
+        [NotNull]
+        Task<string> GetParentZoneAsync(string environment);
+    }
+}
