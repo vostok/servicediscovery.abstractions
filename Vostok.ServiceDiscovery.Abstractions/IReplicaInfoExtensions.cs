8a5b80791f09cd9818705b9fe132d56eeba1bb2e
kungurtsev <kungurtsev@skbkontur.ru>
2019-11-30 13:51:50 +0500

    TryGetUrl extension

---
+++ Vostok.ServiceDiscovery.Abstractions/IReplicaInfoExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using JetBrains.Annotations;
+
+namespace Vostok.ServiceDiscovery.Abstractions
+{
+    [PublicAPI]
+    public static class IReplicaInfoExtensions
+    {
+        /// <summary>
+        /// Tries to parse absolute url from <see cref="IReplicaInfo.Replica"/>.
+        /// </summary>
+        public static bool TryGetUrl([NotNull] this IReplicaInfo replicaInfo, out Uri result) =>
+            Uri.TryCreate(replicaInfo.Replica, UriKind.Absolute, out result);
+    }
+}
