e4dd5ae451b1d097dc354fa70926dcd7d46b70d9
iloktionov <iloktionov@skbkontur.ru>
2020-10-21 17:29:42 +0500

    Changes made during in-person review

--- Vostok.ServiceDiscovery.Abstractions/Models/TagCollection.cs
+++ Vostok.ServiceDiscovery.Abstractions/Models/TagCollection.cs
@@ -2,11 +2,17 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using JetBrains.Annotations;
+using Vostok.Commons.Helpers.Comparers;
 
 namespace Vostok.ServiceDiscovery.Abstractions.Models
 {
+    [PublicAPI]
     public class TagCollection : Dictionary<string, string>
     {
+        private const string DefaultValue = "true";
+        private const string TagsSeparator = "|";
+        private const string TagsKeyValueSeparator = "=";
+
         public TagCollection()
             : base(StringComparer.OrdinalIgnoreCase)
         {
@@ -21,8 +27,10 @@ namespace Vostok.ServiceDiscovery.Abstractions.Models
         {
             try
             {
-                var dict = input.Split(ReplicaTagsStoringConstants.TagsSeparator.ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
-                    .Select(t => t.Split(ReplicaTagsStoringConstants.TagsKeyValueSeparator.ToCharArray(), StringSplitOptions.RemoveEmptyEntries))
+                // CR(iloktionov): Less garbage please :)
+
+                var dict = input.Split(TagsSeparator.ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
+                    .Select(t => t.Split(TagsKeyValueSeparator.ToCharArray(), StringSplitOptions.RemoveEmptyEntries))
                     .Where(t => t.Length == 2)
                     .GroupBy(t => t[0], t => t[1], StringComparer.InvariantCultureIgnoreCase)
                     .ToDictionary(x => x.Key, x => x.First(), StringComparer.InvariantCultureIgnoreCase);
@@ -36,43 +44,21 @@ namespace Vostok.ServiceDiscovery.Abstractions.Models
             }
         }
 
-        public void Add([NotNull] Tag tag)
+        public void Add([NotNull] string key)
         {
-            if (tag == null)
-                throw new ArgumentNullException(nameof(tag));
+            if (key == null)
+                throw new ArgumentNullException(nameof(key));
 
-            Add(tag.Key, tag.Value);
+            Add(key, DefaultValue);
         }
-
-        public IEnumerable<Tag> GetTags()
-            => this.Select(tag => new Tag(tag.Key, tag.Value));
 
         public override string ToString()
-        {
-            var tags = this.Select(x => x.Key + ReplicaTagsStoringConstants.TagsKeyValueSeparator + x.Value);
-            return string.Join(ReplicaTagsStoringConstants.TagsSeparator, tags);
-        }
+            => string.Join(TagsSeparator, this.Select(x => x.Key + TagsKeyValueSeparator + x.Value));
         
         #region Equality members
 
         public bool Equals(TagCollection other)
-        {
-            if (ReferenceEquals(null, other)) 
-                return false;
-            if (ReferenceEquals(this, other)) 
-                return true;
-            if (Count != other.Count) 
-                return false;
-
-            foreach (var pair in this)
-            {
-                if (!other.TryGetValue(pair.Key, out var value)
-                    || !value.Equals(pair.Value, StringComparison.OrdinalIgnoreCase))
-                    return false;
-            }
-
-            return true;
-        }
+            => DictionaryComparer<string, string>.Instance.Equals(this, other);
 
         public override bool Equals(object obj)
         {
@@ -82,23 +68,12 @@ namespace Vostok.ServiceDiscovery.Abstractions.Models
                 return true;
             if (obj.GetType() != GetType()) 
                 return false;
+
             return Equals((TagCollection)obj);
         }
 
         public override int GetHashCode()
-        {
-            unchecked
-            {
-                var hashCode = 397;
-                foreach (var pair in this)
-                {
-                    hashCode = (hashCode * 397) ^ pair.Key.GetHashCode();
-                    hashCode = (hashCode * 397) ^ pair.Value.GetHashCode();
-                }
-
-                return hashCode;
-            }
-        }
+            => DictionaryComparer<string, string>.Instance.GetHashCode(this);
 
         #endregion
     }
