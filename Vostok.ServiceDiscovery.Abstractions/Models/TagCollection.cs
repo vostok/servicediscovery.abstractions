d599b9e5328d5101d0acb085f0e058f21a0bf4e9
Borovsky Arkady <borovskyav@list.ru>
2020-10-21 15:52:46 +0500

    Add Equality methods for TagCollection

--- Vostok.ServiceDiscovery.Abstractions/Models/TagCollection.cs
+++ Vostok.ServiceDiscovery.Abstractions/Models/TagCollection.cs
@@ -8,12 +8,12 @@ namespace Vostok.ServiceDiscovery.Abstractions.Models
     public class TagCollection : Dictionary<string, string>
     {
         public TagCollection()
-            : base(StringComparer.InvariantCultureIgnoreCase)
+            : base(StringComparer.OrdinalIgnoreCase)
         {
         }
 
         public TagCollection(IDictionary<string, string> tagsCollection)
-            : base(tagsCollection, StringComparer.InvariantCultureIgnoreCase)
+            : base(tagsCollection, StringComparer.OrdinalIgnoreCase)
         {
         }
 
@@ -44,13 +44,62 @@ namespace Vostok.ServiceDiscovery.Abstractions.Models
             Add(tag.Key, tag.Value);
         }
 
+        public IEnumerable<Tag> GetTags()
+            => this.Select(tag => new Tag(tag.Key, tag.Value));
+
         public override string ToString()
         {
             var tags = this.Select(x => x.Key + ReplicaTagsStoringConstants.TagsKeyValueSeparator + x.Value);
             return string.Join(ReplicaTagsStoringConstants.TagsSeparator, tags);
         }
+        
+        #region Equality members
 
-        public IEnumerable<Tag> GetTags()
-            => this.Select(tag => new Tag(tag.Key, tag.Value));
+        public bool Equals(TagCollection other)
+        {
+            if (ReferenceEquals(null, other)) 
+                return false;
+            if (ReferenceEquals(this, other)) 
+                return true;
+            if (Count != other.Count) 
+                return false;
+
+            foreach (var pair in this)
+            {
+                if (!other.TryGetValue(pair.Key, out var value)
+                    || !value.Equals(pair.Value, StringComparison.OrdinalIgnoreCase))
+                    return false;
+            }
+
+            return true;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) 
+                return false;
+            if (ReferenceEquals(this, obj)) 
+                return true;
+            if (obj.GetType() != GetType()) 
+                return false;
+            return Equals((TagCollection)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = 397;
+                foreach (var pair in this)
+                {
+                    hashCode = (hashCode * 397) ^ pair.Key.GetHashCode();
+                    hashCode = (hashCode * 397) ^ pair.Value.GetHashCode();
+                }
+
+                return hashCode;
+            }
+        }
+
+        #endregion
     }
 }
